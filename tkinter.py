6a6fd4bbe8d8c3c126095cf799af78fd24d83840
AjkaKrilek <113473323+AjkaKrilek@users.noreply.github.com>
2023-06-21 07:46:13 +0200

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,41 +1,40 @@
 import tkinter as tk
-win = tk.Tk()
-
-at = tk.Label(win,text='koeficient a:')
-at.pack()
-a = tk.Entry(win)
-a.pack()
-
-bt = tk.Label(win,text='koeficient b:')
-bt.pack()
-b = tk.Entry(win)
-b.pack()
-
-ct = tk.Label(win,text='koeficient c:')
-ct.pack()
-c = tk.Entry(win)
-c.pack()
-
 
 def executor():
-    print(a.get(),b.get(),c.get())
     ka = int(a.get())
     kb = int(b.get())
     kc = int(c.get())
-    d = kb**2-4*ka*kc
-    if d<0:
-        v = tk.Label(text='nema riesenie v R')
-        v.pack()
-    elif d==0:
-        vx = tk.Label(win, str(text='x=' + (-kb/(2*ka))))
-        vx.pack()
+    d = kb**2 - 4*ka*kc
+    if d < 0:
+        result_label.config(text="Nema riesenie v R")
+    elif d == 0:
+        result_label.config(text="x: " + str(-kb / (2*ka)))
     else:
-        vy = tk.Label(str(text='x1=' + (-kb + d ** 0.5) / (2 * ka)))
-        vy.pack()
-        vz = tk.Label(str(text='x2=' + (-kb - d ** 0.5) / (2 * ka)))
-        vz.pack()
+        result_label.config(text="x1: " + str((-kb + d ** 0.5) / (2*ka)))
+        result_label.config(text="x2: " + str((-kb - d ** 0.5) / (2*ka)))
 
-button = tk.Button(win,text='Click me!',command=executor)
+win = tk.Tk()
+win.title("Quadratic Equation Solver")
+
+atext = tk.Label(win, text="koeficient a:")
+atext.pack()
+a = tk.Entry(win)
+a.pack()
+
+btext = tk.Label(win, text="koeficient b:")
+btext.pack()
+b = tk.Entry(win)
+b.pack()
+
+ctext = tk.Label(win, text="koeficient c:")
+ctext.pack()
+c = tk.Entry(win)
+c.pack()
+
+button = tk.Button(win, text="hotovko!", command=executor)
 button.pack()
+
+result_label = tk.Label(win, text="")
+result_label.pack()
 
 win.mainloop()
